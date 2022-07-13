1ea35fa0a24bff6a66869f5d4a8e5fd831cf3454
ooyyee <ooyyee@qq.com>
2022-07-13 23:51:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 ###### Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 ###### Modify hostname
 sed -i 's/OpenWrt/K2P/g' package/base-files/files/bin/config_generate
